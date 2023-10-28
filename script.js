fcf335d42e9569264937150ae215881692c87477
Illarion Ivanchenko <larik.maths@gmail.com>
2023-10-27 19:45:41 -0600

    Button

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("👋 🌎");
+
+let root = document.getElementById('root');
+
+const label = ["S", "M", "I", "L", "E"];
+let position = -1;
+
+function handleClick(e) {
+    position = (position + 1) % label.length;
+    e.innerText = label[position];
+}
+
+console.dir(root);
+
+const newButton = document.createElement("button");
+
+root.appendChild(newButton);
+
+newButton.innerText = 'X';
+
+newButton.classList.add("square");
+
+newButton.setAttribute("onClick", "handleClick(this)");
